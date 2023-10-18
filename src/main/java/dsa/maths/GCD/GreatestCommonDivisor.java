856aef88eaf61f1e9fc512fb9853eea6a203a8b2
parulagg27 <parulagg27@gmail.com>
2023-10-19 00:37:11 +0530

    18.10 | Parul | Add brute force approach to find GCD

--- src/main/java/dsa/maths/GCD/GreatestCommonDivisor.java
+++ src/main/java/dsa/maths/GCD/GreatestCommonDivisor.java
@@ -3,17 +3,16 @@ package dsa.maths.GCD;
 public class GreatestCommonDivisor {
 
     /**
-     *
      * Time complexity: ~O(n1 % n2)
      * Space complexity: O(1)
      */
     public static int findGCDByDivisionTillRemainderZero(int n1, int n2) {
-        if (n1 == 0 || n2 == 0){
+        if (n1 == 0 || n2 == 0) {
             return 0;
         }
         int divident = Math.max(n1, n2);
         int divisor = Math.min(n1, n2);
-        while (true){
+        while (true) {
             int remainder = divident % divisor;
             if (remainder == 0) {
                 break;
@@ -21,6 +20,25 @@ public class GreatestCommonDivisor {
             divident = divisor;
             divisor = remainder;
         }
-        return divisor;
+        return Math.abs(divisor);
+    }
+
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    public static int findGCDByBruteForce(int n1, int n2) {
+        if (n1 == 0 || n2 == 0) {
+            return 0;
+        }
+        int i;
+        int gcd = 1;
+
+        for (i = 1; i <= Math.abs(Math.min(n1, n2)); i++) {
+            if ((n1 % i == 0) && (n2 % i == 0)) {
+                gcd = i;
+            }
+        }
+        return gcd;
     }
 }
--- src/test/java/dsa/maths/GCD/GreatestCommonDivisorTest.java
+++ src/test/java/dsa/maths/GCD/GreatestCommonDivisorTest.java
@@ -1,49 +1,99 @@
 package dsa.maths.GCD;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class GreatestCommonDivisorTest {
 
-    @Test
-    void getGCDOf0and0(){
-        int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(0, 0);
+    @Nested
+    class findGCDByDivisionTillRemainderZero {
+        @Test
+        void getGCDOf0and0(){
+            int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(0, 0);
 
-        Assertions.assertEquals(0, output);
+            Assertions.assertEquals(0, output);
+        }
+
+        @Test
+        void shouldReturnGCDas0IfAnyNumberIsZero() {
+            int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(100, 0);
+
+            Assertions.assertEquals(0, output);
+        }
+
+        @Test
+        void shouldReturnGcdForEvenNumbers() {
+            int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(12, 20);
+
+            Assertions.assertEquals(4, output);
+        }
+
+        @Test
+        void shouldReturnGcdForOddNumbers() {
+            int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(3, 17);
+
+            Assertions.assertEquals(1, output);
+        }
+
+        @Test
+        void shouldReturnGcdForPositiveAndNegativeInt() {
+            int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(12, -12);
+
+            Assertions.assertEquals(12, output);
+        }
+
+        @Test
+        void shouldReturnGcdForIntMaxAndIntMin() {
+            int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(Integer.MAX_VALUE, Integer.MIN_VALUE);
+
+            Assertions.assertEquals(1, output);
+        }
     }
 
-    @Test
-    void shouldReturnGCDas0IfAnyNumberIsZero() {
-        int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(100, 0);
+    @Nested
+    class findGCDByBruteForce {
+        @Test
+        void getGCDOf0and0(){
+            int output = GreatestCommonDivisor.findGCDByBruteForce(0, 0);
 
-        Assertions.assertEquals(0, output);
+            Assertions.assertEquals(0, output);
+        }
+
+        @Test
+        void shouldReturnGCDas0IfAnyNumberIsZero() {
+            int output = GreatestCommonDivisor.findGCDByBruteForce(100, 0);
+
+            Assertions.assertEquals(0, output);
+        }
+
+        @Test
+        void shouldReturnGcdForEvenNumbers() {
+            int output = GreatestCommonDivisor.findGCDByBruteForce(12, 20);
+
+            Assertions.assertEquals(4, output);
+        }
+
+        @Test
+        void shouldReturnGcdForOddNumbers() {
+            int output = GreatestCommonDivisor.findGCDByBruteForce(3, 17);
+
+            Assertions.assertEquals(1, output);
+        }
+
+        @Test
+        void shouldReturnGcdForPositiveAndNegativeInt() {
+            int output = GreatestCommonDivisor.findGCDByBruteForce(12, -12);
+
+            Assertions.assertEquals(12, output);
+        }
+
+        @Test
+        void shouldReturnGcdForIntMaxAndIntMin() {
+            int output = GreatestCommonDivisor.findGCDByBruteForce(Integer.MAX_VALUE, Integer.MIN_VALUE);
+
+            Assertions.assertEquals(1, output);
+        }
     }
 
-    @Test
-    void shouldReturnGcdForEvenNumbers() {
-        int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(12, 20);
-
-        Assertions.assertEquals(4, output);
-    }
-
-    @Test
-    void shouldReturnGcdForOddNumbers() {
-        int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(3, 17);
-
-        Assertions.assertEquals(1, output);
-    }
-
-    @Test
-    void shouldReturnGcdForPositiveAndNegativeInt() {
-        int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(12, -12);
-
-        Assertions.assertEquals(-12, output);
-    }
-
-    @Test
-    void shouldReturnGcdForIntMaxAndIntMin() {
-        int output = GreatestCommonDivisor.findGCDByDivisionTillRemainderZero(Integer.MAX_VALUE, Integer.MIN_VALUE);
-
-        Assertions.assertEquals(-1, output);
-    }
 }
