b9386f53a0835ad7003a554ab8fcd9440efb88d8
parulagg27 <parulagg27@gmail.com>
2023-10-21 19:07:05 +0530

    21.10 | Parul | Add logarithm & charArray method to determine armstrong number Correct. time complexity of modulus 10 logic for armstrong

--- src/main/java/dsa/maths/ArmstrongNumber/Armstrong.java
+++ src/main/java/dsa/maths/ArmstrongNumber/Armstrong.java
@@ -1,8 +1,16 @@
 package dsa.maths.ArmstrongNumber;
 
+/**
+ * An Armstrong number is a number that is equal to the sum of its own digits raised to the power of the number of digits.
+ * leetcode Url: <a href="https://leetcode.com/problems/armstrong-number/">...</a>
+ */
 public class Armstrong {
 
-    public static boolean determineArmstrongByDigitCount(int number) {
+    /**
+     * Time complexity: O(log10(n)) - since loop executes k times till dividend reduces to zero. i.e. dividend / 10^k = 1
+     * Space complexity: O(1)
+     */
+    public static boolean determineArmstrongByModulus10(int number) {
         if (number == 0 || number == 1) return true;
         int dividend = number;
         int count;
@@ -18,5 +26,42 @@ public class Armstrong {
             dividend = dividend / 10;
         }
         return sum == number;
+    }
+
+    /**
+     * Time complexity: O(log10(n)) - due to second loop
+     * Space complexity: O(1)
+     */
+    public static boolean determineArmstrongByLogarithm(int number) {
+        if (number == 0 || number == 1){
+            return true;
+        }
+        int count = (int) Math.floor(Math.log10(Math.abs(number))) + 1;
+        int divident = number;
+        int sum = 0;
+        while (divident != 0){
+            int remainder = divident % 10;
+            sum = (int) (sum + Math.pow(remainder, count));
+            divident = divident / 10;
+        }
+        return sum == number;
+    }
+
+    /**
+     * Time complexity: O(log10(n)) = O(k) where k is digit count. k = log(n) + 1
+     * Space complexity: O(log10(n)) = O(k) where k is digit count.
+     */
+    public static boolean determineArmstrongByCharArray(int number) {
+        if (number == 0 || number == 1) return true;
+        String stringNumber = String.valueOf(Math.abs(number));
+        int digitCount = stringNumber.length();
+        int sum = 0;
+        int i;
+        for (i = 0; i < digitCount; i++){
+            int digit = Character.getNumericValue(stringNumber.charAt(i));
+            sum = (int) (sum + Math.pow(digit, digitCount));
+        }
+
+        return sum == Math.abs(number);
     }
 }
--- src/test/java/dsa/maths/ArmstrongNumber/ArmstrongTest.java
+++ src/test/java/dsa/maths/ArmstrongNumber/ArmstrongTest.java
@@ -1,42 +1,125 @@
 package dsa.maths.ArmstrongNumber;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ArmstrongTest {
 
-    @Test
-    void shouldDetermine153AsArmstrongNumber() {
-        boolean output = Armstrong.determineArmstrongByDigitCount(153);
+    @Nested
+    class determineArmstrongByModulus10 {
 
-        Assertions.assertTrue(output);
+        @Test
+        void shouldDetermine153AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByModulus10(153);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldNotDetermine123AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByModulus10(123);
+
+            Assertions.assertFalse(output);
+        }
+
+        @Test
+        void shouldCheckForGivenNegativeIntegerAsArmstrong() {
+            boolean output = Armstrong.determineArmstrongByModulus10(-153);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldDetermine0AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByModulus10(0);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldCheckGivenLargeIntForArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByModulus10(Integer.MAX_VALUE);
+
+            Assertions.assertFalse(output);
+        }
     }
 
-    @Test
-    void shouldNotDetermine123AsArmstrongNumber() {
-        boolean output = Armstrong.determineArmstrongByDigitCount(123);
+    @Nested
+    class determineArmstrongByLogarithm {
 
-        Assertions.assertFalse(output);
+        @Test
+        void shouldDetermine153AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByLogarithm(153);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldNotDetermine123AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByLogarithm(123);
+
+            Assertions.assertFalse(output);
+        }
+
+        @Test
+        void shouldCheckForGivenNegativeIntegerAsArmstrong() {
+            boolean output = Armstrong.determineArmstrongByLogarithm(-153);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldDetermine0AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByLogarithm(0);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldCheckGivenLargeIntForArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByLogarithm(Integer.MAX_VALUE);
+
+            Assertions.assertFalse(output);
+        }
     }
 
-    @Test
-    void shouldCheckForGivenNegativeIntegerAsArmstrong() {
-        boolean output = Armstrong.determineArmstrongByDigitCount(-153);
+    @Nested
+    class determineArmstrongByCharArray {
 
-        Assertions.assertTrue(output);
-    }
+        @Test
+        void shouldDetermine153AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByCharArray(153);
 
-    @Test
-    void shouldDetermine0AsArmstrongNumber() {
-        boolean output = Armstrong.determineArmstrongByDigitCount(0);
+            Assertions.assertTrue(output);
+        }
 
-        Assertions.assertTrue(output);
-    }
+        @Test
+        void shouldNotDetermine123AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByCharArray(123);
 
-    @Test
-    void shouldCheckGivenLargeIntForArmstrongNumber() {
-        boolean output = Armstrong.determineArmstrongByDigitCount(Integer.MAX_VALUE);
+            Assertions.assertFalse(output);
+        }
 
-        Assertions.assertFalse(output);
+        @Test
+        void shouldCheckForGivenNegativeIntegerAsArmstrong() {
+            boolean output = Armstrong.determineArmstrongByCharArray(-153);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldDetermine0AsArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByCharArray(0);
+
+            Assertions.assertTrue(output);
+        }
+
+        @Test
+        void shouldCheckGivenLargeIntForArmstrongNumber() {
+            boolean output = Armstrong.determineArmstrongByCharArray(Integer.MAX_VALUE);
+
+            Assertions.assertFalse(output);
+        }
     }
 }
