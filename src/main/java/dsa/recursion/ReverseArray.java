1fe21f0250008bba65f81fcc2ba0803070da0beb
parulagg27 <parulagg27@gmail.com>
2023-10-28 13:42:31 +0530

    28.10 | Parul | Add recursive approach to reverse array, Refactor. rename variables to more meaningful names

--- src/main/java/dsa/recursion/ReverseArray.java
+++ src/main/java/dsa/recursion/ReverseArray.java
@@ -22,11 +22,11 @@ public class ReverseArray {
      * Space complexity: O(1) - since not using any extra space.
      */
     public static int[] reverseArrayByHalfTraversal(int n, int[] nums) {
-        int index;
-        for (index = 0; index < n / 2; index++) {
-            int temp = nums[index];
-            nums[index] = nums[n - 1 - index];
-            nums[n - 1 - index] = temp;
+        int start;
+        for (start = 0; start < n / 2; start++) {
+            int temp = nums[start];
+            nums[start] = nums[n - 1 - start];
+            nums[n - 1 - start] = temp;
         }
         return nums;
     }
@@ -37,15 +37,32 @@ public class ReverseArray {
      * Space complexity: O(1) - since not using any extra array space.
      */
     public static int[] reverseArrayByTwoPointers(int n, int[] nums) {
-        int i = 0;
-        int j = n - 1;
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
         return nums;
+    }
+
+    /**
+     * Approach: Similar to reverseArrayByTwoPointers but using recursion to iterate from opposite ends of array
+     * Time complexity: O(n) - actual complexity = n/2, since traversing half of array.
+     * Space complexity: O(1) - since not using any extra array space.
+     */
+    public static int[] reverseArrayByRecursion(int n, int[] nums) {
+        return reverseArrayByRecursion(0, n - 1, nums);
+    }
+
+    private static int[] reverseArrayByRecursion(int start, int end, int[] nums) {
+        if (start >= end) return nums;
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        return reverseArrayByRecursion(start + 1, end - 1, nums);
     }
 }
--- src/test/java/dsa/recursion/ReverseArrayTest.java
+++ src/test/java/dsa/recursion/ReverseArrayTest.java
@@ -137,4 +137,48 @@ class ReverseArrayTest {
             Assertions.assertArrayEquals(expected, output);
         }
     }
+
+    @Nested
+    class ReverseArrayByRecursion {
+
+        @Test
+        void shouldReverseArrayContaining1Element() {
+            int[] input = new int[]{1};
+            int[] expected = new int[]{1};
+
+            var output = ReverseArray.reverseArrayByRecursion(1, input);
+
+            Assertions.assertArrayEquals(expected, output);
+        }
+
+        @Test
+        void shouldReverseArrayContainingNoElement() {
+            int[] input = new int[0];
+            int[] expected = new int[0];
+
+            var output = ReverseArray.reverseArrayByRecursion(0, input);
+
+            Assertions.assertArrayEquals(expected, output);
+        }
+
+        @Test
+        void shouldReverseArrayContaining6Elements() {
+            int[] input = new int[]{5, 7, 8, 1, 6, 3};
+            int[] expected = new int[]{3, 6, 1, 8, 7, 5};
+
+            var output = ReverseArray.reverseArrayByRecursion(6, input);
+
+            Assertions.assertArrayEquals(expected, output);
+        }
+
+        @Test
+        void shouldReverseArrayContaining8Elements() {
+            int[] input = new int[]{3, 1, 1, 7, 4, 2, 6, 11};
+            int[] expected = new int[]{11, 6, 2, 4, 7, 1, 1, 3};
+
+            var output = ReverseArray.reverseArrayByRecursion(8, input);
+
+            Assertions.assertArrayEquals(expected, output);
+        }
+    }
 }
