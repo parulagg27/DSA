f9d114c96c44b92bc097f4625019edec4c9742d8
parulagg27 <parulagg27@gmail.com>
2023-11-17 23:34:59 +0530

    17.11 | Parul | LinkedList traversal to check if given value exists [iterative, recursive]

--- src/main/java/dsa/linkedList/traversal/LinkedListValues.java
+++ src/main/java/dsa/linkedList/traversal/LinkedListValues.java
@@ -34,10 +34,10 @@ public class LinkedListValues {
      * @Time_complexity O(n): traversing entire list
      * @Space_complexity O(1): constant space of size 1 [i.e.sum], independent of linked list size
      */
-    public static Integer sumList(Node<Integer> head){
+    public static Integer sumList(Node<Integer> head) {
         int sum = 0;
         Node<Integer> current = head;
-        while (current != null){
+        while (current != null) {
             sum += current.value;
             current = current.next;
         }
@@ -48,9 +48,32 @@ public class LinkedListValues {
      * @Time_complexity O(n): traversing entire list
      * @Space_complexity O(n): Call trace will contain all function calls till last node is reached & value is returned.
      */
-    public static Integer sumListRecursive(Node<Integer> head){
+    public static Integer sumListRecursive(Node<Integer> head) {
         if (head == null) return 0;
         return head.value + sumListRecursive(head.next);
+    }
+
+    /**
+     * @Time_complexity O(n): traversing entire list
+     * @Space_complexity O(1): constant space used with increasing list size
+     */
+    public static <T> boolean linkedListFind(Node<T> head, T target) {
+        Node<T> current = head;
+        while (current != null) {
+            if (target == current.value) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    /**
+     * @Time_complexity O(n)
+     * @Space_complexity O(n)
+     */
+    public static <T> boolean linkedListRecursively(Node<T> head, T target) {
+        if (head == null) return false;
+        if (head.value == target) return true;
+        return linkedListRecursively(head.next, target);
     }
 
     private static List<String> valuesRecursive(Node<String> current, List<String> values) {
--- src/test/java/dsa/linkedList/traversal/LinkedListValuesTest.java
+++ src/test/java/dsa/linkedList/traversal/LinkedListValuesTest.java
@@ -1,14 +1,13 @@
 package dsa.linkedList.traversal;
 
 import dsa.linkedList.Node;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListValuesTest {
 
@@ -193,6 +192,129 @@ class LinkedListValuesTest {
             var output = LinkedListValues.sumListRecursive(x);
 
             assertEquals(42, output);
+        }
+    }
+
+    @Nested
+    class FindTargetValueIteratively {
+
+        @Test
+        void shouldReturnFalseIfLinkedListDoesntContainAnyNode() {
+            var output = LinkedListValues.linkedListFind(null, 1);
+
+            assertFalse(output);
+        }
+
+        @Test
+        void shouldReturnFalseIfTargetValuePresentInLinkedListOfSize1() {
+            var output = LinkedListValues.linkedListFind(new Node<>(42), 100);
+
+            assertFalse(output);
+        }
+
+        @Test
+        void shouldReturnTrueIfTargetValuePresentInLinkedListOfSize1() {
+            var output = LinkedListValues.linkedListFind(new Node<>(42), 42);
+
+            assertTrue(output);
+        }
+
+        @Test
+        void shouldReturnTrueIfTargetValueNotInLinkedListOfStrings() {
+            Node<String> node1 = new Node<>("jason");
+            node1.next = new Node<>("july");
+
+            var output = LinkedListValues.linkedListFind(node1, "jason");
+
+            assertTrue(output);
+        }
+
+        @Test
+        void shouldReturnTrueIfTargetValueNotInLinkedListOfStringsWithSize4() {
+            Node<String> a = new Node<>("a");
+            Node<String> b = new Node<>("b");
+            Node<String> c = new Node<>("c");
+            Node<String> d = new Node<>("d");
+            a.next = b;
+            b.next = c;
+            c.next = d;
+
+            var output = LinkedListValues.linkedListFind(a, "c");
+
+            assertTrue(output);
+        }
+
+        @Test
+        void shouldReturnFalseIfTargetValueNotInLinkedListOfStringsWithSize4() {
+            Node<String> a = new Node<>("a");
+            Node<String> b = new Node<>("b");
+            Node<String> c = new Node<>("c");
+            Node<String> d = new Node<>("d");
+            a.next = b;
+            b.next = c;
+            c.next = d;
+
+            var output = LinkedListValues.linkedListFind(a, "q");
+
+            assertFalse(output);
+        }
+    }
+
+    @Nested
+    class FindTargetValueRecursively {
+
+        @Test
+        void shouldReturnFalseIfTargetValuePresentInLinkedListOfSize1() {
+            var output = LinkedListValues.linkedListRecursively(new Node<>(42), 100);
+
+            assertFalse(output);
+        }
+
+        @Test
+        void shouldReturnTrueIfTargetValuePresentInLinkedListOfSize1() {
+            var output = LinkedListValues.linkedListRecursively(new Node<>(42), 42);
+
+            assertTrue(output);
+        }
+
+        @Test
+        void shouldReturnTrueIfTargetValueNotInLinkedListOfStrings() {
+            Node<String> node1 = new Node<>("jason");
+            node1.next = new Node<>("july");
+
+            var output = LinkedListValues.linkedListRecursively(node1, "jason");
+
+            assertTrue(output);
+        }
+
+        @Test
+        void shouldReturnTrueIfTargetValueNotInLinkedListOfStringsWithSize4() {
+            Node<String> a = new Node<>("a");
+            Node<String> b = new Node<>("b");
+            Node<String> c = new Node<>("c");
+            Node<String> d = new Node<>("d");
+            a.next = b;
+            b.next = c;
+            c.next = d;
+
+            var output = LinkedListValues.linkedListRecursively(a, "c");
+
+            assertTrue(output);
+        }
+
+        @Test
+        void shouldReturnFalseIfTargetValueNotInLinkedListOfStringsWithSize4() {
+            Node<String> a = new Node<>("a");
+            Node<String> b = new Node<>("b");
+            Node<String> c = new Node<>("c");
+            Node<String> d = new Node<>("d");
+            a.next = b;
+            b.next = c;
+            c.next = d;
+
+            var output = LinkedListValues.linkedListRecursively(a, "q");
+
+            assertFalse(output);
         }
     }
 }
