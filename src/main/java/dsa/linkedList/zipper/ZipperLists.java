4bc1b64deb2a448d8fc2bf0f9abc604b4594f6e5
parulagg27 <parulagg27@gmail.com>
2023-11-22 23:09:26 +0530

    22.11 | Parul | Add logic to zipper two linekd lists by shifting current and next pointer

--- src/main/java/dsa/linkedList/zipper/ZipperLists.java
+++ src/main/java/dsa/linkedList/zipper/ZipperLists.java
@@ -4,26 +4,28 @@ import dsa.linkedList.Node;
 
 public class ZipperLists {
 
-    public static <T> Node<T> zipperLists(Node<T> head1, Node<T> head2){
+    /**
+     * @Time_complexity O(min(n, m)) - n, m: size of list1 & list2 respectively
+     * @Space_complexity O(1) - constant space used with increasing list size [zipped in-place]
+     */
+    public static <T> Node<T> zipperListsByShiftingCurrentAndNextPointer(Node<T> head1, Node<T> head2){
         Node<T> current1 = head1;
         Node<T> current2 = head2;
 
         while (current1 != null && current2 != null){
-            Node<T> next1;
-            Node<T> next2;
-            next1 = current1.next;
-            next2 = current2.next;
-            if (current2 != null) current1.next = current2;
+            Node<T> next1 = current1.next;
+            Node<T> next2 = current2.next;
+            current1.next = current2;
             if (next1 != null) current2.next = next1;
             current1 = next1;
             current2 = next2;
         }
-        return current1;
+        return head1;
     }
 
     /**
      * @Time_complexity O(min(n, m)) - n, m: size of list1 & list2 respectively
-     * @Space_complexity O(1) - constant space used with increasing list size
+     * @Space_complexity O(1) - constant space used with increasing list size [zipped in-place]
      * TODO: revisit approach with visualisation
      */
     public static <T> Node<T> zipperByOddEvenAndCurrentPointer(Node<T> head1, Node<T> head2){
