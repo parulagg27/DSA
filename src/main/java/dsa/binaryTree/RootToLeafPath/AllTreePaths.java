558c5ea4c86300cbc55e2068e508499e2199367c
parulagg27 <parulagg27@gmail.com>
2024-02-23 00:14:57 +0530

    22.02.24 | Parul | Add depth first post-order approach to find all tree paths

--- src/main/java/dsa/binaryTree/RootToLeafPath/AllTreePaths.java
+++ src/main/java/dsa/binaryTree/RootToLeafPath/AllTreePaths.java
@@ -4,18 +4,14 @@ import dsa.binaryTree.Node;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class AllTreePaths {
 
     public static <T> List<List<T>> allTreePaths(Node<T> root) {
         if (root == null) return null;
-
         if (root.left == null && root.right == null) {
-            List<T> rootElements = new ArrayList<>();
-            rootElements.add(root.value);
-            return new ArrayList<>(){{
-                add(rootElements);
-            }};
+            return List.of(List.of(root.value));
         }
 
         List<List<T>> leftSubtrees = allTreePaths(root.left);
@@ -23,17 +19,53 @@ public class AllTreePaths {
         List<List<T>> allSubTrees = new ArrayList<>();
 
         if (leftSubtrees != null) {
-            for(List<T> subTree: leftSubtrees) {
-                subTree.add(0, root.value);
-                allSubTrees.add(subTree);
+            for (List<T> subTree : leftSubtrees) {
+                List<T> path = new ArrayList<>();
+                path.add(root.value);
+                path.addAll(subTree);
+                allSubTrees.add(path);
             }
         }
         if (rightSubTrees != null) {
-            for(List<T> subTree: rightSubTrees) {
-                subTree.add(0, root.value);
-                allSubTrees.add(subTree);
+            for (List<T> subTree : rightSubTrees) {
+                List<T> path = new ArrayList<>();
+                path.add(root.value);
+                path.addAll(subTree);
+                allSubTrees.add(path);
             }
         }
         return allSubTrees;
+    }
+
+    public static <T> List<List<T>> viaPostOrder(Node<T> root) {
+        if (root == null) return null;
+        Node<T> current = root;
+        List<List<T>> allPaths = new ArrayList<>();
+        Stack<Node<T>> stack = new Stack<>();
+        List<T> pathTracker = new ArrayList<>();
+
+        while (current != null || !stack.isEmpty()) {
+            if (current != null) {
+                stack.push(current);
+                pathTracker.add(current.value);
+                current = current.left;
+            } else {
+                var top = stack.peek();
+                if (top.right != null) current = top.right;
+                else {
+                    if (top.left == null){
+                        List<T> path = new ArrayList<>(pathTracker);
+                        allPaths.add(path);
+                    }
+                    var node = stack.pop();
+                    pathTracker.remove(pathTracker.size() - 1);
+                    while (!stack.isEmpty() && node == stack.peek().right){
+                        node = stack.pop();
+                        pathTracker.remove(pathTracker.size() - 1);
+                    }
+                }
+            }
+        }
+        return allPaths;
     }
 }
--- src/test/java/dsa/binaryTree/RootToLeafPath/AllTreePathsTest.java
+++ src/test/java/dsa/binaryTree/RootToLeafPath/AllTreePathsTest.java
@@ -122,4 +122,118 @@ class AllTreePathsTest {
             Assertions.assertEquals(expected, output);
         }
     }
+
+    @Nested
+    class ViaPostOrder {
+
+        @Test
+        void shouldReturnAllPathsForBalancedBinaryTree() {
+            Node<String> a = new Node<>("a");
+            Node<String> b = new Node<>("b");
+            Node<String> c = new Node<>("c");
+            Node<String> d = new Node<>("d");
+            Node<String> e = new Node<>("e");
+            Node<String> f = new Node<>("f");
+            a.left = b;
+            a.right = c;
+            b.left = d;
+            b.right = e;
+            c.right = f;
+            //      a
+            //    /   \
+            //   b     c
+            //  / \     \
+            // d   e     f
+
+            var output = AllTreePaths.viaPostOrder(a);
+
+            List<List<String>> expected = List.of(
+                    List.of("a", "b", "d"),
+                    List.of("a", "b", "e"),
+                    List.of("a", "c", "f")
+            );
+            Assertions.assertEquals(expected, output);
+        }
+
+        @Test
+        void shouldReturnAllPathsForTree() {
+            Node<String> a = new Node<>("a");
+            Node<String> b = new Node<>("b");
+            Node<String> c = new Node<>("c");
+            Node<String> d = new Node<>("d");
+            Node<String> e = new Node<>("e");
+            Node<String> f = new Node<>("f");
+            Node<String> g = new Node<>("g");
+            Node<String> h = new Node<>("h");
+            Node<String> i = new Node<>("i");
+            a.left = b;
+            a.right = c;
+            b.left = d;
+            b.right = e;
+            c.right = f;
+            e.left = g;
+            e.right = h;
+            f.left = i;
+            //         a
+            //      /    \
+            //     b      c
+            //   /  \      \
+            //  d    e      f
+            //      / \    /
+            //     g  h   i
+
+            var output = AllTreePaths.viaPostOrder(a);
+
+            List<List<String>> expected = List.of(
+                    List.of("a", "b", "d"),
+                    List.of("a", "b", "e", "g"),
+                    List.of("a", "b", "e", "h"),
+                    List.of("a", "c", "f", "i")
+            );
+            Assertions.assertEquals(expected, output);
+        }
+
+        @Test
+        void shouldReturnAllPathsForLeftSkewedTree() {
+            Node<String> q = new Node<>("q");
+            Node<String> r = new Node<>("r");
+            Node<String> s = new Node<>("s");
+            Node<String> t = new Node<>("t");
+            Node<String> u = new Node<>("u");
+            Node<String> v = new Node<>("v");
+            q.left = r;
+            q.right = s;
+            r.right = t;
+            t.left = u;
+            u.right = v;
+            //      q
+            //    /   \
+            //   r     s
+            //    \
+            //     t
+            //    /
+            //   u
+            //  /
+            // v
+
+            var output = AllTreePaths.viaPostOrder(q);
+
+            List<List<String>> expected = List.of(
+                    List.of("q", "r","t", "u", "v"),
+                    List.of("q", "s")
+            );
+            Assertions.assertEquals(expected, output);
+        }
+
+        @Test
+        void shouldReturnPathForSingleNodeTree() {
+            Node<String> z = new Node<>("z");
+            //    z
+
+            var output = AllTreePaths.viaPostOrder(z);
+
+            var expected = List.of(List.of("z"));
+            Assertions.assertEquals(expected, output);
+        }
+    }
 }
