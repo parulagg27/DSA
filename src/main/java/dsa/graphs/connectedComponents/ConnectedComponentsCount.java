5bc65d6600f450be13912fbd95462398f7af1490
parulagg27 <parulagg27@gmail.com>
2024-03-10 00:24:56 +0530

    09.03.24 | Parul | Add optimised approach to get connected components count in graph [O(n^2) -> O(n)]

--- src/main/java/dsa/graphs/connectedComponents/ConnectedComponentsCount.java
+++ src/main/java/dsa/graphs/connectedComponents/ConnectedComponentsCount.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class ConnectedComponentsCount {
 
     /**
-     * Approach: Find neighbours of each node till and track them in set. Unique set of paths are obtained as result.
+     * Approach: Traverse each node, Find neighbours for each and track them in set. Unique set of paths are obtained as result.
      * @Time_complexity O(V * E) -> loop repeats v times: gets neighbours for each vertex * going over each edge to get all neighbours
      * @Space_complexity O(V + V + V) -> Set to track unique neighbours, Recursion over each node taking stack space + connected components set
      */
@@ -24,12 +24,40 @@ public class ConnectedComponentsCount {
         return connectedComponents.size();
     }
 
-    public static Set<Integer> getNeighbours(Map<Integer, List<Integer>> graph, Integer src) {
+    /**
+     * Approach:
+     * 1) Keep main list to track visited nodes. [to avoid finding neighbours for nodes already visited [i.e. getting same neighbour set]
+     * 2) Loop over each node, i.e., key to start traversal of complete paths
+     * 3) Traversal logic + increment count on 1 complete traversal
+     * @Time_complexity O(E+V) -> since loop do not execute for nodes already visited [so no V*E]
+     * @Space_complexity O(V+V) -> V: to track traversed nodes. V: for recursive traversal of path
+     */
+    public static int connectedComponentsOptimised(Map<Integer, List<Integer>> graph) {
+        Set<Integer> visitedNodes = new HashSet<>();
+        int count = 0;
+        for (int key: graph.keySet()) {
+            if (traversePath(graph, key, visitedNodes)){
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean traversePath(Map<Integer, List<Integer>> graph, int src, Set<Integer> visitedNodes) {
+        if (visitedNodes.contains(src)) return false;
+        visitedNodes.add(src);
+        for (int neighbour: graph.get(src)) {
+            traversePath(graph, neighbour, visitedNodes);
+        }
+        return true;
+    }
+
+    private static Set<Integer> getNeighbours(Map<Integer, List<Integer>> graph, Integer src) {
         Set<Integer> neighbours = new HashSet<>();
         return getNeighbours(graph, src, neighbours);
     }
 
-    public static Set<Integer> getNeighbours(Map<Integer, List<Integer>> graph, Integer src, Set<Integer> neighbours) {
+    private static Set<Integer> getNeighbours(Map<Integer, List<Integer>> graph, Integer src, Set<Integer> neighbours) {
         if (neighbours.contains(src)) return neighbours;
         neighbours.add(src);
         for (Integer neighbour: graph.get(src)) {
